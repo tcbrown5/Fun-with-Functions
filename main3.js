d76fb9aa1b31000877112ff3c0384124d233e9f3
tcbrown5 <107014856+tcbrown5@users.noreply.github.com>
2022-06-20 17:43:39 -0500

    Add files via upload

---
+++ main3.js
@@ -0,0 +1,9 @@
+function myfunctionOne(p1, p2=5){
+    return p1*p2;
+}
+function myfunctionTwo(p1, p2, p3){
+    console.log(p1+p2+p3)
+}
+console.log(myfunctionOne(10))
+console.log(myfunctionOne(10,10))
+myfunctionTwo (1,2,3)
